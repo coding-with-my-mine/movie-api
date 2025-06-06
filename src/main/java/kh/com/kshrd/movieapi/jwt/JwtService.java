29757b76039c014eda704e2b180ab6f29ef0a02f
Sovannakk <sovannak.khengg@gmail.com>
2025-06-06 08:02:22 +0700

    init

--- src/main/java/kh/com/kshrd/movieapi/jwt/JwtService.java
+++ src/main/java/kh/com/kshrd/movieapi/jwt/JwtService.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Component
 public class JwtService {
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60; //5 hour
+    public static final long JWT_TOKEN_VALIDITY = 30L * 24L * 60L * 60L; // Approximately 1 month (30 days)
     public static final String SECRET = "FVPr6Q/fVlHGZkElZubC0Zaxv657dPUfDQ4o9DADjSin7+uST1d2A5klMWrMK8fmSl3doyf2wn5zj56VC+qqCg==";
 
     private String createToken(Map<String, Object> claim, String subject) {
