c1d3924e16e809b7f688c6fc3ae4fe6cb1fd0db5
Sovannakk <sovannak.khengg@gmail.com>
2025-06-12 11:58:14 +0700

    init

--- src/main/java/kh/com/kshrd/movieapi/MovieApiApplication.java
+++ src/main/java/kh/com/kshrd/movieapi/MovieApiApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +15,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 title = "Movie Booking API",
                 version = "1.0",
                 description = "API documentation for the Movie Booking Management System"
+        ),
+        servers = @Server(
+                url = "/"
         )
 )
 @SecurityScheme(
